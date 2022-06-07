10bbcce451dd836421c9dec6a872a69f0bb97595
Wandia E Mugo <wwandia173@gmail.com>
2022-06-07 21:42:23 +0300

    commit tree 6a97d00a176d20ee9ca40cf528934b32b0b85f9f

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1=62;
+const num2=1;
+let multiply=num1*num2;
+console.log (multiply);
+
+let random=Math.floor(Math.random()*100);
+console.log (random);
+
+const num3=13;
+const num4=9;
+let mod=num3%num4;
+console.log (mod);
+
+let max=Math.max(5,8,15,18,20);
+console.log (max);
